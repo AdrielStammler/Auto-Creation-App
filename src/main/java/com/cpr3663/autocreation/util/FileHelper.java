e147c46e5b1d43797d3e88089be21ce548da2c67
Adriel <stammler.adriel@gmail.com>
2026-06-22 21:06:17 -0700

    Made functional shortcuts, made popup and file helpers

---
+++ src/main/java/com/cpr3663/autocreation/util/FileHelper.java
@@ -0,0 +1,15 @@
+package com.cpr3663.autocreation.util;
+
+public class FileHelper {
+    public static void create() {
+
+    }
+
+    public static void open() {
+
+    }
+
+    public static void save() {
+
+    }
+}
