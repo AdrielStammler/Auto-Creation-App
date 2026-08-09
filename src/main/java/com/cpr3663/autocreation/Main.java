b2521e206fda17d1c671ac7b44ff6dfc98d6f5e9
Adriel <stammler.adriel@gmail.com>
2026-08-09 04:30:29 -0700

    Fix my oops

--- src/main/java/com/cpr3663/autocreation/Main.java
+++ src/main/java/com/cpr3663/autocreation/Main.java
@@ -54,7 +54,7 @@ public class Main extends Application {
 
         // Creating scene and setting stage properties
         Scene scene = new Scene(root, Constants.Stage.WIDTH, Constants.Stage.HEIGHT);
-        stage.initStyle(StageStyle.UNIFIED);
+        stage.initStyle(StageStyle.UNDECORATED);
         stage.setTitle(Constants.Stage.TITLE);
         stage.setScene(scene);
         stage.show();
